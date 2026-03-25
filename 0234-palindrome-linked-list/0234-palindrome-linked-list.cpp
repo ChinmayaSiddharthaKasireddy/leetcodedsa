aced68af5a3cd280edcdc6c6042d8cef4ce61928
siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-03-25 15:20:38 +0530

    Time: 4 ms (50.02%), Space: 118.1 MB (65.51%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        // Step 1: Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Step 3: Compare both halves
+        ListNode* first = head;
+        ListNode* second = prev;
+        
+        while (second) {
+            if (first->val != second->val)
+                return false;
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
