b0b7469045538e480087ecec3ca69346d5051f67
siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-08-27 15:18:59 +0530

    Time: 3 ms (51.97%), Space: 19.4 MB (5.27%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,5 +1,9 @@
-class Solution(object):
-    def findTheDifference(self, s, t):
-        return chr(sum(map(ord, t)) - sum(map(ord, s)))
-
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count_s = Counter(s)
+        count_t = Counter(t)
+        for char in count_t:
+            if count_t[char] > count_s[char]:
+                return char
         
